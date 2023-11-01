8b385065d0b3fdb2825a2872c30730ecb56f74ad
bondiano <vasiliy.kuzenkov@dualbootpartners.com>
2023-11-01 16:12:58 +0700

    init

---
+++ src/schema.ts
@@ -0,0 +1,13 @@
+export type TypeOf = unknown;
+
+export const string = {};
+
+export const number = {};
+
+export const unknown = {};
+
+export const array = {};
+
+export const object = {};
+
+export type { TypeOf as infer };
